07b9de27ac42c09aca013dd2dfffe8ac0d859c56
Mason Hall <71787606+hallmasonc@users.noreply.github.com>
2025-01-24 12:28:53 -0500

    New additions to post-install.sh and updated TODO.md to reflect changes. Include cloning and install of most additional tools and configs.

--- post-install.sh
+++ post-install.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
 
-# Variables
+## Variables
+dot="https://github.com/hallmasonc/dotfiles"
+dotDir="~/.dotfiles"
+rofi="https://github.com/adi1090x/rofi.git"
+rofiDir="~/.config/rofi.git"
 yay="https://aur.archlinux.org/yay.git"
 yayDir="~/yay"
 
-# Uncomment multilib repo
-sudo sed -i "/\[multilib\]/,/Include/"'s/^#*//' /etc/pacman.conf
-
-# Force pacman database update
-sudo pacman -Syy
-
-# Install pacman packages
-sudo pacman -S --needed - < ./pacman.txt
-
-# Function
+## Functions
+# git clone function
 cloneFunction () {
     git clone $1 $2
 }
 
-# Install yay as AUR helper
+## Main
+# Clone items
+
+# yay
 cloneFunction $yay $yayDir
 
+# dotfiles
+cloneFunction $dot $dotDir
+
+# rofi
+cloneFunction $rofi $rofiDir
+
+# Begin pacman
+# enable multilib repo
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#*//' /etc/pacman.conf
+
+# force pacman database update
+sudo pacman -Syy
+
+# install pacman packages
+sudo pacman -S --needed - < ./pacman.txt
+
+# Begin yay
 if [ ! -d "$yayDir" ]; then
     echo "failed to clone yay repo"
 else
-    # Move directory and build
+    # move directory and build
     cd $yayDir
     makepkg --noconfirm -si
 
-    # Move back to previous directory
+    # move back to previous directory
     cd -
 
-    # Remove yay directory (cleanup)
+    # remove yay directory (cleanup)
     rm -rf $yayDir
 
-    # Install yay packages
+    # install yay packages
     yay -S --needed - < ./yay.txt
 fi
 
-# Add flathub as remote for flatpaks
+# Begin flatpak
+# add flathub as remote for flatpaks
 flatpak remote-add --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install flatpaks
+# install flatpaks
 xargs flatpak --user install -y < ./flatpak.txt
 
-# Enable lightdm service
+# Misc.
+# stow dotfiles
+bash ~/.dotfiles/stowit.sh
+
+# alacritty theme
+bash ~/.config/alacritty/get-themes.sh 
+
+# Services
+# enable lightdm service
 sudo systemctl enable lightdm.service
